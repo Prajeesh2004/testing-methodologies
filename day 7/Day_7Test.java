efdc07a0ebc0f5117e87311a14a8657b578471b5
Prajeesh2004 <113415319+Prajeesh2004@users.noreply.github.com>
2023-06-03 16:17:37 +0530

    Add files via upload

---
+++ day 7/Day_7Test.java
@@ -0,0 +1,22 @@
+package com.example.testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Day_7Test {
+
+	@Test
+	public void compare() {
+
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.wpeverest.com/user-registration/guest-registration-form/");
+		String a = driver.getTitle();
+		Assert.assertEquals(a, "Guest Registration Form – User Registration");
+	}
+}
